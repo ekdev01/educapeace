6fb87de20952d78916f5c11824851afcdfeec1e9
r.desire92@gmail.com <r.desire92@gmail.com>
2018-02-13 20:29:00 +0100

    Setting up the shop page

--- src/EduBundle/Controller/ShopController.php
+++ src/EduBundle/Controller/ShopController.php
@@ -8,8 +8,24 @@
 
 namespace EduBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class ShopController
+class ShopController extends Controller
 {
-	
+	/**
+	 * @Route("/shop", name="shop_page")
+	 */
+	public function indexAction(Request $request)
+	{
+		$this->container->get('edu.translation')->changeLanguage($request);
+		$trans = $this->get('translator');
+		
+		return $this->render('EduBundle:Shop:index.html.twig', array(
+			'lang'       => $request->getLocale(),
+			'page_titre' => (string) $trans->trans('page_title_shop'),
+			'theme'      => 'shop'
+		));
+	}
 }
